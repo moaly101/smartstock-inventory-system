bce56962ef237a29ece96469548cae147fed2a7d
moaly101 <Mohammad.Alyounes@hhu.de>
2026-03-27 18:05:44 +0100

    initial spring boot project setup

---
+++ src/main/java/org/example/smartstocksystem/SmartstockSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.smartstocksystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartstockSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartstockSystemApplication.class, args);
+    }
+
+}
